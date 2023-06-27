bb85847dd16822bf92721aca1369cdafe0dc9981
Victor Hideki Ogawa <victor.ogawa@gmail.com>
2023-06-27 12:31:41 -0400

    v0.05.1

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -3,45 +3,66 @@ import Image from "next/image";
 import { styled } from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-flex-direction: row;
-background-color:#FFFF44;
-align-items: center;
-padding-top: 10px;
-padding-bottom: 10px;
-padding-left: 20px;
-padding-right: 20px;
-justify-content: space-between;
-`
+  display: flex;
+  flex-direction: column;
+  background-color: #ffff44;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
 
 export const Logo = styled(Image)`
-background-color: transparent;
-width: 20%;
-height: auto;
-`
+  background-color: transparent;
+  width: 60%;
+  height: auto;
+
+  @media (min-width: 768px) {
+    width: 30%;
+  }
+
+  @media (min-width: 1024px) {
+    width: 20%;
+  }
+`;
+
+export const Text = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: transparent;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 2%;
+`;
 
 export const PolTerms = styled.div`
-background-color: transparent;
-`
+  background-color: transparent;
+`;
 
 export const Copyright = styled.label`
-text-align:center;
-background-color: transparent;
-`
+  text-align: center;
+  background-color: transparent;
+  font-size: ${px2vw(10, 320)};
+  width: 40%;
+
+  @media (min-width: 768px) {
+    font-size: ${px2vw(10, 768)};
+  }
+`;
 
 export const Li = styled.li`
-background-color: transparent;
-font-size: ${px2vw(10, 320)};
-padding: 2px;
+  background-color: transparent;
+  font-size: ${px2vw(10, 320)};
+  padding: 2px;
 
-@media (min-width: 768px) {
+  @media (min-width: 768px) {
     font-size: ${px2vw(10, 768)};
-}
+  }
 
-@media (min-width: 1024px) {
+  @media (min-width: 1024px) {
     font-size: ${px2vw(10, 1024)};
-}
-`
+  }
+`;
 
-export const A = styled.a`
-`
+export const A = styled.a``;
