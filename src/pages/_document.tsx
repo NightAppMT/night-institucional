906e3a6fb218cd0c7d9341768e1a10efbda379ce
Victor Hideki Ogawa <victor.ogawa@gmail.com>
2023-08-03 16:55:01 -0400

    add meta verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="er1amkpxu8vlm8acpg6c48mywp8ypj"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
