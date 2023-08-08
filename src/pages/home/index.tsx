dd58480b9b6dce24954edb1307e5d4c4d08d4c98
Victor Hideki Ogawa <victor.ogawa@gmail.com>
2023-08-08 17:31:46 -0400

    test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -25,9 +25,9 @@ export default function Home() {
   }, []);
   return (
     <Container>
-      {/* <HeaderComponent /> */}
+      <HeaderComponent />
       <BannerComponent />
-      {/* <FillFirst />
+      <FillFirst />
       <InsideComponent />
       <Image
         src={"/logo.svg"}
@@ -60,8 +60,8 @@ export default function Home() {
         }}
       />
       <PartnersComponent />
-      <FooterComponent /> */}
-      {/* <div
+      <FooterComponent />
+      <div
         style={{
           display: "flex",
           width: "100vw",
@@ -87,7 +87,7 @@ export default function Home() {
             />
           </>
         )}
-      </div> */}
+      </div>
     </Container>
   );
 }
